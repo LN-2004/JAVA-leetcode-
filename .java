79879fb03c9177b84483b937f8e47d7582cbe2f8
LAKSHMI NARAYANAN R <lakshminarayanan2907@gmail.com>
2025-08-08 19:22:32 +0530

    Update .java

--- .java
+++ .java
@@ -77,3 +77,29 @@ class Solution {
         
     }
 }
+//COUNT AND SAY
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1) return "1";
+        String result="1";
+        for(int i=2;i<=n;i++){
+            StringBuilder sb=new StringBuilder();
+            char prevchar=result.charAt(0);
+            int count=1;
+        for(int j=1;j<result.length();j++){
+            if(result.charAt(j)==prevchar){
+                count++;
+            }else{
+                sb.append(count).append(prevchar);
+                prevchar=result.charAt(j);
+                count=1;
+            }
+        } 
+        sb.append(count).append(prevchar);
+        result= sb.toString();   
+        }
+        
+        return result;
+    }
+    
+}
